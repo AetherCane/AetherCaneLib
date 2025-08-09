387b8bf1d482e42db3bc3e7c3e95055d9de2fba1
Weesl <sefasam553498@gmail.com>
2025-08-09 15:58:42 +0300

    change dep type for libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.aethercane"
-version = "1.1"
+version = "1.2"
 
 repositories {
     mavenCentral()
@@ -23,11 +23,11 @@ repositories {
 }
 
 dependencies {
-    api("com.j256.ormlite:ormlite-jdbc:6.1")
-    api("com.fasterxml.jackson.core:jackson-databind:2.19.0")
-    api("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.19.0")
-    api("dev.triumphteam:triumph-gui:3.1.11")
-    api("dev.triumphteam:triumph-cmd-bukkit:2.0.0-BETA-3")
+    implementation("com.j256.ormlite:ormlite-jdbc:6.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.19.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.19.0")
+    implementation("dev.triumphteam:triumph-gui:3.1.11")
+    implementation("dev.triumphteam:triumph-cmd-bukkit:2.0.0-BETA-3")
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.6")
 }
