dc3fb234bb7acc7552bf04e4ce0eb05ff8ecbfe5
Birol <1olozturk09@gmail.com>
2025-08-10 15:29:46 +0300

    Add MiniMessage field with italics disabled

--- src/main/java/com/aethercane/aethercanelib/util/StringsUtil.java
+++ src/main/java/com/aethercane/aethercanelib/util/StringsUtil.java
@@ -3,6 +3,7 @@ package com.aethercane.aethercanelib.util;
 import com.aethercane.aethercanelib.AetherCaneLib;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
@@ -10,12 +11,18 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import javax.annotation.Nullable;
+import java.util.Collection;
+import java.util.List;
 import java.util.logging.Level;
+import java.util.stream.Collectors;
 
 public class StringsUtil {
 
     private final static LegacyComponentSerializer ampersandSerializer = LegacyComponentSerializer.legacyAmpersand();
     private final static LegacyComponentSerializer sectionSerializer = LegacyComponentSerializer.legacySection();
+
+    private final static MiniMessage MINI_MESSAGE = MiniMessage.builder()
+            .postProcessor(component -> component.decoration(TextDecoration.ITALIC, false)).build();
 
     public static Component applyPlaceholder(String input, Player player) {
         if (!Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
@@ -35,18 +42,16 @@ public class StringsUtil {
     }
 
     public static Component applyColor(String input, TagResolver... resolvers) {
-        MiniMessage mm = MiniMessage.miniMessage();
         Component legacy = ampersandSerializer.deserialize(input);
-        String minimessage = mm.serialize(legacy).replace("\\", "");
-        return mm.deserialize(minimessage, resolvers);
+        String minimessage = MINI_MESSAGE.serialize(legacy).replace("\\", "");
+        return MINI_MESSAGE.deserialize(minimessage, resolvers);
     }
 
     private static Component applyColor(Component component, TagResolver... resolvers) {
-        MiniMessage mm = MiniMessage.miniMessage();
         String input = sectionSerializer.serialize(component);
         Component legacy = ampersandSerializer.deserialize(input);
-        String minimessage = mm.serialize(legacy).replace("\\", "");
-        return mm.deserialize(minimessage, resolvers);
+        String minimessage = MINI_MESSAGE.serialize(legacy).replace("\\", "");
+        return MINI_MESSAGE.deserialize(minimessage, resolvers);
     }
 
     public static Component applyString(String input, @Nullable Player player, TagResolver... resolvers) {
@@ -56,5 +61,11 @@ public class StringsUtil {
         }
         component = applyColor(component, resolvers);
         return component;
+    }
+
+    public static List<Component> applyStringCollection(Collection<String> input, @Nullable Player player, TagResolver... resolvers) {
+        return input.stream()
+                .map(line -> applyString(line, player, resolvers))
+                .toList();
     }
 }
