d2dd1abdeb178145884da00e50e6fd396937d82a
Birol <1olozturk09@gmail.com>
2025-08-20 21:17:25 +0300

    add: align direction for location

--- src/main/java/com/aethercane/aethercanelib/util/LocationUtil.java
+++ src/main/java/com/aethercane/aethercanelib/util/LocationUtil.java
@@ -3,8 +3,13 @@ package com.aethercane.aethercanelib.util;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.block.BlockFace;
 
 public class LocationUtil {
+
+    private static final BlockFace[] RADIAL = {BlockFace.NORTH, BlockFace.NORTH_EAST,
+            BlockFace.EAST, BlockFace.SOUTH_EAST, BlockFace.SOUTH, BlockFace.SOUTH_WEST,
+            BlockFace.WEST, BlockFace.NORTH_WEST};
 
     public static String fromLocation(Location location) {
         return location.getWorld().getName() + "," +
@@ -29,5 +34,25 @@ public class LocationUtil {
                 Double.parseDouble(arguments[2]),
                 Float.parseFloat(arguments[3]),
                 Float.parseFloat(arguments[4]));
+    }
+
+    protected Location alignDirection(Location loc) {
+        Location location = loc.clone();
+        location.setPitch(0);
+        switch (yawToFace(location.getYaw())) {
+            case NORTH -> location.setYaw(0);
+            case NORTH_EAST -> location.setYaw(45);
+            case EAST -> location.setYaw(90);
+            case SOUTH_EAST -> location.setYaw(135);
+            case SOUTH -> location.setYaw(180);
+            case SOUTH_WEST -> location.setYaw(225);
+            case WEST -> location.setYaw(270);
+            case NORTH_WEST -> location.setYaw(360);
+        }
+        return location;
+    }
+
+    private static BlockFace yawToFace(float yaw) {
+        return RADIAL[Math.round(yaw / 45) & 0x7];
     }
 }
